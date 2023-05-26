4f930e6f376d6d9a7b8e9990b4f583c0d07c7302
Isuru <isurudulakshana012@gmail.com>
2023-05-26 07:09:39 +0530

    Initial commit

---
+++ client1/src/controller/Client1Controller.java
@@ -0,0 +1,33 @@
+package controller;
+
+import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXTextArea;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
+public class Client1Controller {
+
+    @FXML
+    private JFXListView<?> chatListView;
+
+    @FXML
+    private JFXTextArea txtArea;
+
+    @FXML
+    void btnSend(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionKeyReleasedTxtArea(KeyEvent event) {
+
+    }
+
+    @FXML
+    void onMouseClickedImageSelect(MouseEvent event) {
+
+    }
+
+}
